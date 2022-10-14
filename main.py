0d765540c7adc8e075e07c4088b4f6547ac65f51
Sabyasachi <54863241+Sabyasachi6215@users.noreply.github.com>
2022-10-14 22:51:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app=Flask(__name__)
 @app.route('/', methods=['GET'])
 
 def home():
-    return "Hello World- I am using Github Actions with this app  Hi !!! Hello !!!"
+    return "Hello World- I am using Github Actions with this app-- check githubactions"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5000)
